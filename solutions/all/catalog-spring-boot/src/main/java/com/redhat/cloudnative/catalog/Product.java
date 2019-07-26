679167785743a9447317cdb60708edcf0088a0f7
tosin Akinosho <takinosh@redhat.com>
2019-07-26 16:49:19 -0400

    updating vertx and springboot solution

--- solutions/all/catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/Product.java
+++ solutions/all/catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/Product.java
@@ -10,53 +10,53 @@ import javax.persistence.UniqueConstraint;
 @Entity
 @Table(name = "PRODUCT", uniqueConstraints = @UniqueConstraint(columnNames = "itemId"))
 public class Product implements Serializable {
-	
-	@Id
-	private String itemId;
-	
-	private String name;
-	
-	private String description;
-	
-	private double price;
+  
+  @Id
+  private String itemId;
+  
+  private String name;
+  
+  private String description;
+  
+  private double price;
 
-	public Product() {
-	}
-	
-	public String getItemId() {
-		return itemId;
-	}
+  public Product() {
+  }
+  
+  public String getItemId() {
+    return itemId;
+  }
 
-	public void setItemId(String itemId) {
-		this.itemId = itemId;
-	}
+  public void setItemId(String itemId) {
+    this.itemId = itemId;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-	public double getPrice() {
-		return price;
-	}
+  public double getPrice() {
+    return price;
+  }
 
-	public void setPrice(double price) {
-		this.price = price;
-	}
+  public void setPrice(double price) {
+    this.price = price;
+  }
 
-	@Override
-	public String toString() {
-		return "Product [itemId=" + itemId + ", name=" + name + ", price=" + price + "]";
-	}
+  @Override
+  public String toString() {
+    return "Product [itemId=" + itemId + ", name=" + name + ", price=" + price + "]";
+  }
 }
